0626a70a9d7819e16c4827669174a3b87cdf2639
Michael Johnston <lastobelus@mac.com>
2009-06-10 08:27:25 -0700

    Initial commit to trac2bex.

---
+++ test/trac2bex_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Trac2bexTest < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
